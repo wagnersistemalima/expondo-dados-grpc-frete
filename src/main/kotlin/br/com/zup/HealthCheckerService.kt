dde2b70e3f9611e05745a32e359ad91b38848d74
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-05-14 19:56:59 -0300

    implementando end point healthcheck, servico de monitoramento para GRPC frete

---
+++ src/main/kotlin/br/com/zup/HealthCheckerService.kt
@@ -0,0 +1,33 @@
+package br.com.zup
+
+import io.grpc.health.v1.HealthCheckRequest
+import io.grpc.health.v1.HealthCheckResponse
+import io.grpc.health.v1.HealthGrpc
+import io.grpc.stub.StreamObserver
+import javax.inject.Singleton
+
+// esta anotação é para a classe ser reconhecida pelo Micronaut
+
+@Singleton
+class HealthCheckerService: HealthGrpc.HealthImplBase() {
+
+    override fun check(request: HealthCheckRequest?, responseObserver: StreamObserver<HealthCheckResponse>?) {
+        responseObserver?.onNext(
+            HealthCheckResponse
+                .newBuilder()
+                .setStatus(HealthCheckResponse.ServingStatus.SERVING)
+                .build()
+        )
+
+        responseObserver?.onCompleted()
+    }
+
+    override fun watch(request: HealthCheckRequest?, responseObserver: StreamObserver<HealthCheckResponse>?) {
+        responseObserver?.onNext(
+            HealthCheckResponse
+                .newBuilder()
+                .setStatus(HealthCheckResponse.ServingStatus.SERVING)
+                .build()
+        )
+    } 
+}
